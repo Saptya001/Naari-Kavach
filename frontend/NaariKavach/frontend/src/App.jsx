0ca312ba6d35fa703424724e7bb3689dfd992b5b
Saptya001 <saptyadeepsomlaptop1@gmail.com>
2025-08-21 22:57:56 +0530

    21.08.2025
    hlww hii

--- frontend/NaariKavach/frontend/src/App.jsx
+++ frontend/NaariKavach/frontend/src/App.jsx
@@ -3,15 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
+import Navbar from './components/Navbar';
+import SOSButton from './components/SOSButton';
+import MapScreen from './components/MapScreen';
+import Chatbot from './components/Chatbot';
 
 const App = () => {
     return (
         <Router>
-            <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route path="/about" component={AboutPage} />
-                <Route component={NotFoundPage} />
-            </Switch>
+            <div>
+                <Navbar />
+                <SOSButton />
+                <MapScreen />
+                <Chatbot />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route path="/about" component={AboutPage} />
+                    <Route component={NotFoundPage} />
+                </Switch>
+            </div>
         </Router>
     );
 };
